53e0c6c6eae3ffa79823cd5aabe0ae222a99fa2a
Samu Vartiainen <samuvartiainen@gmail.com>
2020-07-02 15:25:38 +0300

    trying with bot commando.Client()

--- bot.js
+++ bot.js
@@ -7,33 +7,33 @@ const prefix = '-';
  
 const fs = require('fs');
  
-client.commands = new Discord.Collection();
+bot.commands = new Discord.Collection();
  
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for(const file of commandFiles){
     const command = require(`./commands/${file}`);
  
-    client.commands.set(command.name, command);
+    bot.commands.set(command.name, command);
 }
  
  
-client.once('ready', () => {
+bot.once('ready', () => {
     console.log('ValBot is online!');
 });
  
-client.on('message', message =>{
+bot.on('message', message =>{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
  
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
  
     if(command === 'ping'){
-        client.commands.get('ping').execute(message, args);
+        bot.commands.get('ping').execute(message, args);
     }
 
     else if(command === 'join'){
-        client.commands.get('join').execute(message, args);
+        bot.commands.get('join').execute(message, args);
     }
 });
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+bot.login(process.env.BOT_TOKEN);
