772467140ab04c2a154169a8b6b5b49151ccba20
Samu Vartiainen <samuvartiainen@gmail.com>
2020-07-02 15:59:59 +0300

    trying with bot commando.Client()

--- bot.js
+++ bot.js
@@ -1,42 +1,37 @@
 const Discord = require('discord.js');
-const Commando = require('discord.js-commando');
-const bot = new Commando.Client();
 const client = new Discord.Client();
-const path = require('path');
+ 
 const prefix = '-';
  
 const fs = require('fs');
-bot.registry.registerGroup('music', 'Music');
-bot.registry.registerDefaults();
-bot.registry.registerCommandsIn(path.join(__dirname, '/commands'));
-/*
-bot.commands = new Discord.Collection();
+ 
+client.commands = new Discord.Collection();
  
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for(const file of commandFiles){
     const command = require(`./commands/${file}`);
  
-    bot.commands.set(command.name, command);
+    client.commands.set(command.name, command);
 }
- */
  
-bot.on('ready', () => {
+ 
+client.once('ready', () => {
     console.log('ValBot is online!');
 });
  
-bot.on('message', message =>{
+client.on('message', message =>{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
  
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
  
     if(command === 'ping'){
-        bot.commands.get('ping').execute(message, args);
+        client.commands.get('ping').execute(message, args);
     }
 
-    else if(command === 'join'){
-        bot.commands.get('join').execute(message, args);
-    }
+    // else if(command === 'join'){
+     //   client.commands.get('join').execute(message, args);
+    //  }
 });
 // THIS  MUST  BE  THIS  WAY
-bot.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
